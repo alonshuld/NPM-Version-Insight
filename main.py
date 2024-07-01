e22d643115dc314992c5a8964934f5ad06926b47
alonsd <alonshuld@gmail.com>
2024-07-01 21:55:48 +0300

    getting each readme file

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from sys import argv
+import re
 import requests
 
 
 def fetch_npm_package(package_name, version=None):
     url = f"https://registry.npmjs.org/{package_name}"
+    if version:
+        url += f"/{version}"
     response = requests.get(url)
     if response.status_code == 200:     # package found
         package_info = response.json()
-        if version:     # specific version
-            if version in package_info['versions']:
-                return package_info['versions'][version]
-            else:
-                return None
-        else:
-            return package_info
+        return package_info
     else:
         print("Package not found!")
         return None
@@ -31,8 +28,23 @@ def get_previous_versions(package_name, amount_of_versions):
     return versions[:amount_of_versions]
 
 
-def get_readme_for_version(package):
-    return package.get('readme', '')
+def get_repository_url(package_data):
+    repository = package_data.get('repository', {})
+    if isinstance(repository, dict):
+        url = repository.get('url', '')
+    else:
+        url = repository
+    return re.sub(r'^git\+|\.git$', '', url)  # Clean up the URL
+
+
+def fetch_readme_from_github(repo_url, version):
+    readme_url = f"{repo_url}/raw/v{version}/README.md"
+    response = requests.get(readme_url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        print(f"Failed to fetch README for version {version}. Status code: {response.status_code}")
+        return None
 
 
 def main():
@@ -44,18 +56,19 @@ def main():
     if amount_of_versions <= 1:   # check at least 2 versions
         print("Invalid execution! The amount of versions has to be greater than 1")
         return
-    package = fetch_npm_package(package_name, '3.3.0')
-    print(package.keys())
-    # latest_versions = get_previous_versions(package_name, amount_of_versions)
-    # if latest_versions == None:
-    #     return
-    # latest_packages = []
-    # for version in latest_versions:
-    #     package = fetch_npm_package(package_name, version)
-    #     if package == None:
-    #         return
-    #     latest_packages.append(package)
-    # print(get_readme_for_version(latest_packages[0]))
+    latest_versions = get_previous_versions(package_name, amount_of_versions)
+    if latest_versions == None:
+        return
+    readmes = []
+    for version in latest_versions:
+        package = fetch_npm_package(package_name, version)
+        if package == None:
+            return
+        readme = fetch_readme_from_github(get_repository_url(package), version)
+        if readme is not None:
+            readmes.append(readme)
+    print(len(readmes))
+
     
     
     

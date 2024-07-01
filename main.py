04348b3a72968cbaab9040f3df4c314a5888adc1
alonsd <alonshuld@gmail.com>
2024-07-01 20:11:38 +0300

    getting each fetch

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def fetch_npm_package(package_name, version=None):
             if version in package_info['versions']:
                 return package_info['versions'][version]
             else:
-                print("Version not found!")
                 return None
         else:
             return package_info
@@ -20,28 +19,44 @@ def fetch_npm_package(package_name, version=None):
         return None
 
 
-def get_previous_versions(package_name, versions):
+def get_previous_versions(package_name, amount_of_versions):
     package_info = fetch_npm_package(package_name)
     if package_info == None:
         return None
     if package_name:
-        versions = list(package_info['versions'].keys())
-    print(versions)
+        versions = list(package_info['versions'].keys())[::-1]  # reverse it to go from latest to oldest
+        if len(versions) < amount_of_versions:      # checks if there is enough versions
+            print("The package has only {} versions".format(len(versions)))
+            return None
+    return versions[:amount_of_versions]
+
+
+def get_readme_for_version(package):
+    return package.get('readme', '')
 
 
 def main():
-    if len(argv) != 3:   # the name and at the amount of versions to check
+    if len(argv) != 3 or not argv[2].isnumeric():   # checks that the file executed with the right amount of args and format
         print("Invalid execution! python main.py {npm_package} {number_of_last_versions}")
         return
     package_name = argv[1]
-    versions = int(argv[2])
-    if versions <= 1:   # check at least 2 versions
+    amount_of_versions = int(argv[2])
+    if amount_of_versions <= 1:   # check at least 2 versions
         print("Invalid execution! The amount of versions has to be greater than 1")
         return
-    list_versions = get_previous_versions(package_name, versions)
-    if list_versions == None:
-        return
-    print(list_versions)
+    package = fetch_npm_package(package_name, '3.3.0')
+    print(package.keys())
+    # latest_versions = get_previous_versions(package_name, amount_of_versions)
+    # if latest_versions == None:
+    #     return
+    # latest_packages = []
+    # for version in latest_versions:
+    #     package = fetch_npm_package(package_name, version)
+    #     if package == None:
+    #         return
+    #     latest_packages.append(package)
+    # print(get_readme_for_version(latest_packages[0]))
+    
     
     
 
